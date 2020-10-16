2fe107b4845a00548a813e17b544ba70bb662097
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2020-10-16 23:55:28 +0200

    test

--- pkg/etcd/etcd.go
+++ pkg/etcd/etcd.go
@@ -496,6 +496,10 @@ func (e *ETCD) removePeer(ctx context.Context, id, address string) error {
 				return err
 			}
 			if u.Hostname() == address {
+				if e.address == address {
+					logrus.Infof("")
+					continue
+				}
 				logrus.Infof("Removing name=%s id=%d address=%s from etcd", member.Name, member.ID, address)
 				_, err := e.client.MemberRemove(ctx, member.ID)
 				if err != nil {
