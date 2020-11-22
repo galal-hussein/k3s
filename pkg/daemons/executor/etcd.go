313885950dc746387cb93cfd84cf0ceb45301fde
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2020-11-23 00:47:00 +0200

    Add backup with retention

--- pkg/daemons/executor/etcd.go
+++ pkg/daemons/executor/etcd.go
@@ -3,16 +3,11 @@
 package executor
 
 import (
+	"github.com/rancher/k3s/pkg/util"
 	"github.com/sirupsen/logrus"
 	"go.etcd.io/etcd/embed"
 	"go.etcd.io/etcd/etcdserver"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"sort"
-	"strconv"
 	"strings"
-	"time"
 )
 
 func (e Embedded) CurrentETCDOptions() (InitialOptions, error) {
@@ -37,13 +32,11 @@ func (e Embedded) ETCD(args ETCDConfig) error {
 		select {
 		case err := <-etcd.Server.ErrNotify():
 			var backupdatadir string
-			logrus.Info(etcdserver.ErrMemberRemoved)
 			if strings.Contains(err.Error(), etcdserver.ErrMemberRemoved.Error()) {
-				if backupdatadir, err = RemoveDataDir(args.DataDir); err != nil {
+				if backupdatadir, err = util.BackupDirWithRetention(args.DataDir, 5); err != nil {
 					logrus.Fatalf("Failed to remove old etcd datadir: %v", err)
 				}
 			}
-			logrus.Infof("here2")
 			logrus.Fatalf("etcd data dir was moved to %s - please re run k3s and it will join the cluster", backupdatadir)
 		case <-etcd.Server.StopNotify():
 			logrus.Fatalf("etcd stopped")
@@ -52,42 +45,4 @@ func (e Embedded) ETCD(args ETCDConfig) error {
 		}
 	}()
 	return nil
-}
-
-// RemoveDataDir will move the datadir to a backup dir
-// and will keep only maxBackupRetention of datadirs
-func RemoveDataDir(datadir string) (string, error) {
-	logrus.Infof("in remove data dir")
-	backupDataDir := datadir + "-backup-" + strconv.Itoa(int(time.Now().Unix()))
-	if _, err := os.Stat(datadir); err != nil {
-		return "", nil
-	}
-	files, err := ioutil.ReadDir(filepath.Dir(datadir))
-	if err != nil {
-		return "", err
-	}
-	sort.Slice(files, func(i,j int) bool{
-		return files[i].ModTime().After(files[j].ModTime())
-	})
-	count := 0
-	for _, f := range files {
-		logrus.Infof("here")
-		count++
-		logrus.Info(f)
-		logrus.Info(f.IsDir())
-		if strings.HasPrefix(f.Name(), "etcd-backup") && f.IsDir() {
-			if count > 5 {
-				logrus.Infof("deleting")
-				if err := os.RemoveAll(filepath.Join(filepath.Dir(datadir), f.Name())); err != nil {
-					return "", err
-				}
-			}
-		}
-	}
-	// move the data directory to a temp path
-	logrus.Info(backupDataDir, datadir)
-	if err := os.Rename(datadir, backupDataDir); err != nil {
-		return "", err
-	}
-	return backupDataDir, nil
 }
